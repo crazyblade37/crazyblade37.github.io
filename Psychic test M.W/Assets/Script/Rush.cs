be200f2a77f109bee7a05aba5704db157ce99645
crazyblade37 <walkermalik21@gmail.com>
2017-10-09 19:24:58 -0400

    new game horse in space

---
+++ Psychic test M.W/Assets/Script/Rush.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rush : MonoBehaviour {
+	public AudioClip impact;
+	//http://soundbible.com/1723-Horse-Blow.html - recorded by stephan schutze the audio clip
+	AudioSource hono;
+
+	public Transform target; //public holder 
+	Rigidbody seeker;
+	public float booster;
+	// Use this for initialization
+	void Start () {
+		seeker = GetComponent<Rigidbody> ();
+		hono = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 targetDir = Vector3.Normalize (target.position - transform.position);
+		seeker.AddForce (targetDir * booster);
+	}
+
+void OnTriggerEnter(Collider other) {
+		hono.PlayOneShot(impact, 0.7F);
+}
+}
